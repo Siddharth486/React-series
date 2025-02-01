46af448dfa987bd9330234b4bbe99046c7601bd1
Siddharth486 <siddharthsaniyan123@gmail.com>
2025-02-01 23:43:46 +0530

    Ep-3 How to use React component

--- app.js
+++ app.js
@@ -14,12 +14,35 @@ import ReactDOM from 'react-dom/client';
 // console.log(heading);
 
 // React component
-const Heading2 = ()=>(<div><h2>Shorter Heading</h2></div>)
-/**component inside another component => @component_composition */ 
-const Heading= ()=>(<div><h1>Bigger Heading</h1><Heading2/></div>)
+
+const Heading2 = () => (<div><h2>Shorter Heading</h2></div>)
+/**component inside another component => @component_composition */
+
+/**@oneway to render component as @Selector **/
+// const Heading = () => (
+//   <div>
+//     <h1>Bigger Heading</h1>
+//     <Heading2 />
+//   </div>
+// )
+/** @2ndway as @tag "" */
+// const Heading = () => (
+//   <div>
+//     <h1>Bigger Heading</h1>
+//     <Heading2></Heading2>
+//   </div>
+// )
+
+/** @3rdway as normal @function as at the end of the day @Reactcomponent is just a normal js function ***/
+const Heading = () => (
+    <div>
+      <h1>Bigger Heading</h1>
+      {Heading2()}
+    </div>
+  )
 
 // React.createElement => Object=>  HTML(DOM)
-const parent = React.createElement('h1',{},'Hello World using React');
+const parent = React.createElement('h1', {}, 'Hello World using React');
 /**** @ReactDOM handles the rendering of react component */
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Heading/>);
+root.render(<Heading />);

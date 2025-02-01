86a23d4da374ef6cd662c957d74c915af7e41f76
Siddharth486 <siddharthsaniyan123@gmail.com>
2025-02-01 23:59:25 +0530

    Ep-3 very Important part

--- app.js
+++ app.js
@@ -5,44 +5,29 @@ import ReactDOM from 'react-dom/client';
 // first for Human understanding as it is read by a lot of
 // developers
 
-// JSX
-// const heading = (
-//   <div>
-//     <h1>heading</h1>
-//   </div>
-// );
-// console.log(heading);
+/************ @JSX => @React.createElement(converted to babel) => React Element(plain js object rendered into DOM) ************/
+/** How JSX and React Elements Work Together
+1. JSX is HTML like syntax
+2. React element is plain javascript object 
+const element = {
+  type: 'h1',
+  props: {
+    className: 'greeting',
+    children: 'Hello, World!'
+  }
+};
+React uses this object to create and update the actual DOM. **/
 
-// React component
 
-const Heading2 = () => (<div><h2>Shorter Heading</h2></div>)
-/**component inside another component => @component_composition */
-
-/**@oneway to render component as @Selector **/
-// const Heading = () => (
-//   <div>
-//     <h1>Bigger Heading</h1>
-//     <Heading2 />
-//   </div>
-// )
-/** @2ndway as @tag "" */
-// const Heading = () => (
-//   <div>
-//     <h1>Bigger Heading</h1>
-//     <Heading2></Heading2>
-//   </div>
-// )
-
-/** @3rdway as normal @function as at the end of the day @Reactcomponent is just a normal js function ***/
-const Heading = () => (
-    <div>
-      <h1>Bigger Heading</h1>
-      {Heading2()}
-    </div>
-  )
+const heading = (
+  <div>
+    <h1>heading</h1>
+  </div>
+);
+console.log(heading);
 
 // React.createElement => Object=>  HTML(DOM)
-const parent = React.createElement('h1', {}, 'Hello World using React');
+const parent = React.createElement('h1',{},'Hello World using React');
 /**** @ReactDOM handles the rendering of react component */
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Heading />);
+root.render(heading);
